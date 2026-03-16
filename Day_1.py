76e841a31d746f730af0bf0ee6a77a06c372a1ee
Muwiii16 <gonzalesluigixd@gmail.com>
2026-03-16 22:28:19 +0800

    Completed day 1 & 2 and now currently studying the 3rd day of python

---
+++ Day_1.py
@@ -0,0 +1,15 @@
+print("Name: Luigi Gonzales")
+print("Country: Philippines")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('Asabeneh'))
+print(type([1, 2, 3]))
+print(type({'name': 'Asabeneh'}))
+
+
+radius = 10
+area = 3.14 * radius**2
+print("The area of the circle is :", area)
