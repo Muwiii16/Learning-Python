eeb2ea58db2bd081b5ab59498319c82438bf3f9b
Luigi <gonzalesluigixd@gmail.com>
2026-03-24 12:59:33 +0800

    Pushed the updated activities, will continue later after class

--- Day_06/task_manager.py
+++ Day_06/task_manager.py
@@ -2,8 +2,10 @@ my_tasks = []
 for i in range(3):
     new_item = input("Enter a new task: ")
     my_tasks.append(new_item)
-    
-count = 1
-for item in my_tasks:
-    print(f"{count}. {item}")
-    count += 1
+
+print(f"You have {len(my_tasks)} tasks to do!")
+for index, tasks in enumerate(my_tasks, start = 1):
+    print(f"{index}. {tasks}")
+
+done_task=int(input("Which task number did you finish?"))
+my_tasks.pop(done_task-1)
